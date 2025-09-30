d3df04cc7afd5cf0b2d806baecb40b2bf99df0de
David Flores <a00573996@tec.mx>
2025-09-30 07:12:11 -0600

    Update win_canvas.py

--- APPODS/app/win_canvas.py
+++ APPODS/app/win_canvas.py
@@ -1,21 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
 
 def open_win_canvas(parent: tk.Tk):
     win = tk.Toplevel(parent)
-    win.title("Canvas (Dibujo)")
-    win.geometry("480x340")
+    win.title("Reporte gráfico de gastos")
+    win.geometry("1920x1080")
 
     frm = ttk.Frame(win, padding=12)
     frm.pack(fill="both", expand=True)
 
-    canvas = tk.Canvas(frm, width=440, height=240, bg="white")
-    canvas.pack()
+  
+    categorias = ["Comida", "Renta", "Gustos", "Transporte", "Servicios"]
+    valores = [2500, 5000, 1500, 1200, 800]
 
-    # Dibujos de ejemplo
-    canvas.create_rectangle(20, 20, 120, 80, outline="black", width=2)
-    canvas.create_oval(150, 20, 220, 90, fill="lightblue")
-    canvas.create_line(240, 30, 360, 90, width=3)
-    canvas.create_text(220, 140, text="¡Hola Canvas!", font=("Segoe UI", 12, "bold"))
+    
+    fig, ax = plt.subplots(figsize=(6, 4))
+    ax.bar(categorias, valores, color=["#FF9999", "#66B2FF", "#99FF99", "#FFD966", "#C266FF"])
+    ax.set_title("Reporte de Gastos", fontsize=14, fontweight="bold")
+    ax.set_ylabel("Cantidad ($)")
+    ax.set_xlabel("Categorías")
 
+ 
+    canvas = FigureCanvasTkAgg(fig, master=frm)
+    canvas.draw()
+    canvas.get_tk_widget().pack(fill="both", expand=True)
+
+    
     ttk.Button(frm, text="Cerrar", command=win.destroy).pack(pady=8, anchor="e")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()  
+    open_win_canvas(root)
+    root.mainloop()
